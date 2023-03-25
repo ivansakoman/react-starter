4fa56b5af66418ecde47198975a497db651191c4
ivan.sakoman <saka1993@hotmail.com>
2023-03-25 21:58:54 +0100

    Initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import logo from "./../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div>
+        <img className="header__logo" src={logo} alt="Logo" />
+      </div>
+      <div className="header__actions">
+        <nav className="header__nav">
+          <a className="header__nav__item" href="">
+            Home
+          </a>
+          <a className="header__nav__item" href="">
+            About
+          </a>
+        </nav>
+        <div>IS</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
